5c6197c26804c2cb37149ed4e80a27d9501b5bc4
devkgn88 <devkgn88@gmail.com>
2025-06-17 11:27:21 +0900

    [김가남] Wrapper 클래스 생성하여 요청 가로채기

--- lec03_filter/src/main/java/com/gn/common/filter/MsgFilter.java
+++ lec03_filter/src/main/java/com/gn/common/filter/MsgFilter.java
@@ -2,11 +2,14 @@ package com.gn.common.filter;
 
 import java.io.IOException;
 
+import com.gn.common.wrapper.MsgRequestWrapper;
+
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 
 public class MsgFilter implements Filter{
 
@@ -15,7 +18,13 @@ public class MsgFilter implements Filter{
 			throws IOException, ServletException {
 		request.setCharacterEncoding("UTF-8");
 		System.out.println("[MsgFilter]요청 가로챔");
-		chain.doFilter(request, response);
+		
+		// HttpServletRequest로 캐스팅하고 Wrapper로 감쌈
+        MsgRequestWrapper wrappedRequest = new MsgRequestWrapper((HttpServletRequest) request);
+
+        // 다음 서블릿/필터에게 넘김
+        chain.doFilter(wrappedRequest, response);
+        
 		System.out.println("[MsgFilter]응답 가로챔");
 		
 	}
