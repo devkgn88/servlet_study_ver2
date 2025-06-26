a50d26b9527ba4056f97a22c1a0d78cf0f4ba876
devkgn88 <devkgn88@gmail.com>
2025-06-26 21:36:18 +0900

    [김가남] AutoCommit 여부를 변경할 수 있도록 수정

--- lec07_mybatis/src/main/java/com/gn/common/db/SessionTemplate.java
+++ lec07_mybatis/src/main/java/com/gn/common/db/SessionTemplate.java
@@ -9,22 +9,26 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class SessionTemplate {
-	public static SqlSession getSqlSession() {
+	public static SqlSession getSqlSession(boolean autoCommit) {
 		
 		SqlSession session= null;
 		try {
 			// 1. mybatis-config.xml의 설정 정보를 InputStream을 통해 읽어옴
 			String path = "/mybatis-config.xml";
 			InputStream is = Resources.getResourceAsStream(path);
+			
 			// 2. SqlSessionFactoryBuilder 객체 생성
 			SqlSessionFactoryBuilder sfb = new SqlSessionFactoryBuilder();
+			
 			// 3. SqlSessionFactoryBuilder 객체의 build() 메소드 활용
 			// SqlSessionFactory 객체 생성
 			SqlSessionFactory factory = sfb.build(is);
+			
 			// 4. SqlSessionFactory 객체의 openSession() 메소드 활용
 			// SqlSession 객체 생성
-			// 매개변수로 AutoCommit 여부 지정(기본값 : false)
-			session = factory.openSession(true);
+			// 매개변수로 AutoCommit 여부 지정
+			// 
+			session = factory.openSession(autoCommit);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
